0ac76b29319037f5b339c1b006a6677f2df04aff
Phil Lamplugh <phillip.lamplugh@gmail.com>
2016-12-07 13:05:52 -0800

    update solution

---
+++ main.js
@@ -0,0 +1,34 @@
+var addNoteBtn = document.querySelector('#add-note');
+var board = document.querySelector('.board');
+var noteTotal = document.querySelector('#note-total');
+
+var renderNote = function(){
+  return '<div class="note"><div class="controls"><span class="remove">x</span></div><p class="content" contenteditable="true"></p></div>';
+}
+
+var createNote = function(evt) {
+  var note = renderNote();
+  board.innerHTML += note;
+  updateTotal();
+}
+
+var removeNote = function(evt) {
+  var node = evt.target;
+  if ( !node.classList.contains('remove') ) {
+    return false;
+  }
+  node.closest('.note').remove();
+  updateTotal();
+}
+
+var countNotes = function() {
+  return document.querySelectorAll('.note').length;
+}
+
+var updateTotal = function() {
+  var total = countNotes();
+  noteTotal.textContent = total;
+}
+
+addNoteBtn.addEventListener('click', createNote);
+board.addEventListener('click', removeNote);
