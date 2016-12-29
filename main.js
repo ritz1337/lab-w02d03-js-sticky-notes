7e230ac6a05bf58a47ca300bb949a5629283c659
Phil Lamplugh <phillip.lamplugh@gmail.com>
2016-12-29 13:40:57 -0800

    update solution

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var addNoteBtn = document.querySelector('#add-note');
-var board = document.querySelector('.board');
-var noteTotal = document.querySelector('#note-total');
+var $addNoteBtn = document.querySelector('#add-note');
+var $board = document.querySelector('.board');
+var $noteTotal = document.querySelector('#note-total');
 
 var renderNote = function(){
   return '<div class="note"><div class="controls"><span class="remove">x</span></div><p class="content" contenteditable="true"></p></div>';
@@ -8,7 +8,7 @@ var renderNote = function(){
 
 var createNote = function(evt) {
   var note = renderNote();
-  board.innerHTML += note;
+  $board.innerHTML += note;
   updateTotal();
 }
 
@@ -27,8 +27,8 @@ var countNotes = function() {
 
 var updateTotal = function() {
   var total = countNotes();
-  noteTotal.textContent = total;
+  $noteTotal.textContent = total;
 }
 
-addNoteBtn.addEventListener('click', createNote);
-board.addEventListener('click', removeNote);
+$addNoteBtn.addEventListener('click', createNote);
+$board.addEventListener('click', removeNote);
